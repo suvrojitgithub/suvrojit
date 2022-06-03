30edc69170b8afba34317390a043e782dd1ad61d
suvrojitgithub <102938018+suvrojitgithub@users.noreply.github.com>
2022-06-03 16:14:20 +0530

    pytestintegration

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 import pytest
 
-@pytest.fixture()
-def setup():
-    print("Trial Trial")
-
-def testmenthod(setup):
-    print("Trial Trial")
-
-def testmethod2(setup):
-    print("Trial Trial")
+print ("Hello from Pytest")
